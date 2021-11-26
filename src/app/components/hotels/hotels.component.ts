667270a69ce1f9977d8dbfe1739be45dd549770c
Yassine Sboui <yassine.sboui@aleriaconsulting.eu>
2021-11-26 13:34:01 +0100

    hotels

--- src/app/components/hotels/hotels.component.ts
+++ src/app/components/hotels/hotels.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Dentination } from 'src/app/models/dentination';
+import { DentinationService } from 'src/app/services/dentination.service';
 
 @Component({
   selector: 'app-hotels',
@@ -8,10 +10,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HotelsComponent implements OnInit {
   identifiant: any;
-  constructor(private activatedRoute:ActivatedRoute) { }
+  destination: Dentination;
+  destinationSelect() {
+    this.DentinationService.getOneDentination(this.identifiant).subscribe(
+      (response) => {
+        this.destination=response;
+        return this.destination ;
+
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  constructor(private activatedRoute:ActivatedRoute,private DentinationService: DentinationService,) { }
 
   ngOnInit(): void {
     this.identifiant = this.activatedRoute.snapshot.params['id'];
+    this.destinationSelect();
   }
 
 }
