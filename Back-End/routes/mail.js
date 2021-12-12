de8a4c59d89375cb69e14c4cceb7f4854870a35d
Yassine Sboui <yassine.sboui@aleriaconsulting.eu>
2021-12-13 00:24:24 +0100

    finish

--- Back-End/routes/mail.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const mailCtrl = require('../controllers/mail');
-
-
-router.post('/', mailCtrl.sendmail);
-router.post('/sendresult/', mailCtrl.sendresult);
-
-module.exports = router;
